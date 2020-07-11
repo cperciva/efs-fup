58b51e757e276a3b4ca122595838a36031cdf1a2
Colin Percival <cperciva@tarsnap.com>
2020-07-10 20:07:19 -0700

    Report on http user-agents too

--- www-logprocess.sh
+++ www-logprocess.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+echo "Paths:"
 cat /var/log/nginx/access.log.0 |
     cut -f 7 -d ' ' |
     cut -f 1-2 -d / |
+    sort |
+    uniq -c
+
+echo "User-Agents:"
+cat /var/log/nginx/access.log.0 |
+    cut -f 4 -d '"' |
     sort |
     uniq -c

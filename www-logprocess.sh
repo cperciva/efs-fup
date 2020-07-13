9a3bd27f9edcd4f9b86c7dc7646724e7435aded1
Colin Percival <cperciva@tarsnap.com>
2020-07-12 17:52:20 -0700

    More www log analysis

--- www-logprocess.sh
+++ www-logprocess.sh
@@ -7,8 +7,30 @@ cat /var/log/nginx/access.log.0 |
     sort |
     uniq -c
 
+echo
 echo "User-Agents:"
 cat /var/log/nginx/access.log.0 |
     cut -f 6 -d '"' |
+    sort |
+    uniq -c
+
+grep '"portsnap (' /var/log/nginx/access.log.0 > /root/portsnap-access.log
+
+echo
+echo "Portsnap latest.ssl user-agents:"
+grep 'GET /latest.ssl' /root/portsnap-access.log |
+    cut -f 6 -d '"' |
+    sort |
+    uniq -c
+
+echo
+echo "Snapshot fetches:"
+grep 'GET /s/' /root/portsnap-access.log | wc -l
+
+echo
+echo "Text patches:"
+grep 'GET /tp/' /root/portsnap-access.log |
+    cut -f 7 -d ' ' |
+    cut -f 3 -d '/' |
     sort |
     uniq -c

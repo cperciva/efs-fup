e30728b511279151b7ca58ebeb64bbefcb5905c1
Colin Percival <cperciva@tarsnap.com>
2020-07-14 21:08:59 -0700

    Gather more useful portsnap usage statistics

--- www-logprocess.sh
+++ www-logprocess.sh
@@ -14,23 +14,48 @@ cat /var/log/nginx/access.log.0 |
     sort |
     uniq -c
 
+# Gather portsnap statistics
 grep '"portsnap (' /var/log/nginx/access.log.0 > /root/portsnap-access.log
-
-echo
-echo "Portsnap latest.ssl user-agents:"
-grep 'GET /latest.ssl' /root/portsnap-access.log |
-    cut -f 6 -d '"' |
+cp /local0/ps-mirror/www/indextimes /root/indextimes
+grep 'GET /tp/' /root/portsnap-access.log |
+    cut -f 2,6 -d '"' |
+    cut -f 3- -d '/' |
+    sed -e 's/-/|/; s/\./|/; s/"/|/' |
+    cut -f 1,2,4 -d '|' |
     sort |
-    uniq -c
+    join -t '|' -o 1.2,1.3,2.2 - /local0/ps-mirror/www/indextimes |
+    sort |
+    join -t '|' -o 1.2,1.3,2.2 - /local0/ps-mirror/www/indextimes |
+    sed -E 's/([^|]*)\|(.*)/\2|\1/' |
+    sed -E 's/portsnap \([^,]+, (.*)\)/\1/' |
+    sort -k3 -t '|' > /root/indexupdates
+( cat /root/indexupdates; echo "0|0|0" ) |
+    tr '|' ' ' |
+    while read A B R; do
+	if [ "$R" != "$LR" ]; then
+		if ! [ -z "$LR" ]; then
+			echo "$T|$LR"
+		fi
+		T=0
+	fi
+	T=$((T + B - A))
+	LR="$R"
+done > /root/portsnap-usage-by-version
 
 echo
-echo "Snapshot fetches:"
+echo "Portsnap snapshot fetches:"
 grep 'GET /s/' /root/portsnap-access.log | wc -l
 
 echo
-echo "Text patches:"
-grep 'GET /tp/' /root/portsnap-access.log |
-    cut -f 7 -d ' ' |
-    cut -f 3 -d '/' |
-    sort |
-    uniq -c
+echo "Seconds of updates fetched by FreeBSD version:"
+cat /root/portsnap-usage-by-version
+
+echo
+echo "Total days of updates fetched:"
+cat /root/portsnap-usage-by-version |
+    tr '|' ' ' |
+    while read A R; do
+	T=$((T + A))
+	expr $T / 86400 || true
+    done |
+    tail -1

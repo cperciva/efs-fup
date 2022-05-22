578ab61238e9568ef32f48fd3caa281b7e272627
Colin Percival <cperciva@tarsnap.com>
2022-05-22 12:01:17 -0700

    Disable FreeBSD Update status check temporarily

--- status.sh
+++ status.sh
@@ -12,9 +12,9 @@ if [ `find /local0/ps-mirror/initialized -mtime -6h 2>/dev/null | wc -l` -ne 1 ]
 fi
 
 # FreeBSD Update bits are not synched or more than 6h old
-if [ `find /local0/fu-mirror/initialized -mtime -6h 2>/dev/null | wc -l` -ne 1 ]; then
-	NSTATUS=NO
-fi
+#if [ `find /local0/fu-mirror/initialized -mtime -6h 2>/dev/null | wc -l` -ne 1 ]; then
+#	NSTATUS=NO
+#fi
 
 # Do we need to turn things off?
 if [ "${NSTATUS}${CSTATUS}" = "NOYES" ]; then
